5787fcf369206d0902652d6476b111c60e73ec40
learn <learn@learn13.local>
2017-10-12 13:16:39 -0700

    Second challenge added

---
+++ HiLo.js
@@ -0,0 +1,38 @@
+// Hi Lo Game
+
+// var num = parseInt(Math.random() * 100);
+// var counter = 0;
+// var name;
+// function getName() {
+//   name = document.getElementById("nameField").value;
+//   console.log(name);
+// }
+// function hiLo(x) {
+//   //var name = prompt("What is your name?");
+//   console.log(num);
+//   console.log(name);
+//   var guess = document.getElementById("inputField").value;
+//   console.log(guess);
+//   if (counter < 6 || num != guess) {
+//       if (guess > num) {
+//         document.getElementById("outputText").innerHTML = guess + " Too High! Guess again";
+//         // guess = prompt("Too High! Guess again:");
+//         document.getElementById("inputField").value = "";
+//         counter = counter + 1;
+//       } else if (guess < num) {
+//         document.getElementById("outputText").innerHTML = guess + " Too Low! Guess again";
+//         //guess = prompt("Too Low! Guess again:");
+//         document.getElementById("inputField").value = "";
+//         counter = counter + 1;
+//       }
+//       if (counter === 6) {
+//       alert("Too many guesses! You lose!");
+//     }
+//       if (guess == num && counter != 6) {
+//       alert("You Win " + name + "!");
+//     }
+//   }
+// }
+
+
+//Username and Password
